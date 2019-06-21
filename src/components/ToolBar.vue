18d32eee0a0ce890cab7b25bdca079df5b439690
Flpichon <franckiiboss@gmail.com>
2019-06-21 20:48:12 +0200

    css

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -3,7 +3,7 @@
       <v-navigation-drawer
         :clipped="clipped" v-model="drawer" enable-resize-watcher :hide-overlay=false app dark
       >
-              <v-toolbar flat class="transparent">
+        <v-toolbar flat class="transparent">
           <v-list class="pa-0">
             <v-list-tile avatar>
               <v-list-tile-avatar>
@@ -11,12 +11,12 @@
               </v-list-tile-avatar>
             </v-list-tile>
           </v-list>
-        </v-toolbar>
+        </v-toolbar >
           <v-divider></v-divider>
           <v-list-tile class="slow-transition m-2 btn"
             v-for="item in menu"
             :key="item.title"
-            :to="item.link"
+            @click="Redirect(item.link)"
           >
             <v-list-tile-action>
               <v-icon>{{ item.icon }}</v-icon>
@@ -26,9 +26,9 @@
             </v-list-tile-content>
           </v-list-tile>
       </v-navigation-drawer>
-    <v-toolbar status fixed app :clipped-left="clipped">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title >Gestion d'élèves</v-toolbar-title>
+    <v-toolbar status fixed app :clipped-left="clipped"  class="deep-purple darken-2">
+        <v-toolbar-side-icon class="white--text" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title class="white--text font-weight-thin">Gestion d'élèves</v-toolbar-title>
     </v-toolbar>
       </v-content>
 </template>
@@ -51,6 +51,9 @@
     methods: {
         menuItems() {
         return this.menu;
+        },
+        Redirect(link) {
+          this.$router.push(link) 
         }
     }
   }
@@ -63,8 +66,8 @@
    }
    .v-navigation-drawer{
      background-image: url(/src/assets/8196.jpg);
-    background-size:auto;
-    text-align: center;
+      background-size:400%;
+      text-align: center;
    }
 
 .slow-transition {
@@ -75,13 +78,13 @@
 	box-shadow: 0 10px 50px 0 rgba(0, 0, 0, 0.3);
 }
 .btn{
-  background-color: #E8513C;
+  background-color: rgba(142, 93, 182, 0.664);
   width: 85%;
   color : #ffffff !important;
   text-decoration: none !important;
 }
 .btn:hover{
-    background-color:#FF8050;
+    background-color:rgba(179, 165, 243, 0.513);;
     width: 90%;
 }
 </style>
